a73ef9d4840128d3787d8f649f2da2104b938b3e
msetu1 <msetu5763@gmail.com>
2025-01-09 21:07:03 +0600

    mongodb connected done

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const port = process.env.PORT || 6000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dthbdpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // collection
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Hotel Management Server..");
+});
+
+app.listen(port, () => {
+  console.log(`Hotel Management is running on port ${port}`);
+});
